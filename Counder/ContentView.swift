47cb1db45378f4cd19ff60b00574515e1125889f
maxxwell011 <119656808+maxxwell011@users.noreply.github.com>
2022-12-28 02:44:46 +0900

    Update Counder

---
+++ Counder/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Counder
+//
+//  Created by Sameer Nawaz on 05/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
